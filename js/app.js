0ee07b34f53cabc6906b48adc6945b0c93f13118
mlmedia <millionlightsmedia@gmail.com>
2015-07-20 13:51:03 -0400

    - Getting User Input section up to Enable Upvoting

--- js/app.js
+++ js/app.js
@@ -4,7 +4,24 @@ var app = angular.module('flapperNews', []);
 /* setup the main controller */
 app.controller('MainCtrl', [
     '$scope',
-    function($scope){
+    function($scope)
+    {
         $scope.test = 'Hello world!';
+        $scope.posts = [
+            {title:'post 1', upvotes: 5},
+            {title:'post 2', upvotes: 2},
+            {title:'post 3', upvotes: 15},
+            {title:'post 4', upvotes: 9},
+            {title:'post 5', upvotes: 4}
+        ];
+        $scope.addPost = function( )
+        {
+            if ( !$scope.title || $scope.title == '' )
+            {
+                return;
+            }
+            $scope.posts.push({title:$scope.title, upvotes:0});
+            $scope.title = '';
+        }
     }
 ]);
